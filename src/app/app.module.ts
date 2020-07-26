a8952b9a44c5e08ad7be52940819bb1c3437e8e9
Amanda Harris <alece.fwb@gmail.com>
2020-07-26 16:04:52 -0500

    adding new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavigationComponent
+    NavigationComponent,
+    ContactPageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
