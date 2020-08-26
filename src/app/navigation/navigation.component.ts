b025434293d61c4603faf6fc76d72659ca251830
Amanda Harris <alece.fwb@gmail.com>
2020-08-25 20:34:03 -0500

    working on navigation design

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -11,18 +11,8 @@ export class NavigationComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.navigationActivation();
+ 
   }
 
-  navigationActivation() {
-    var menuToggle = document.querySelector('#menuToggle');
-    var menu = document.querySelector('#menu');
-    menuToggle.addEventListener('click', function(){
-      if(menu.classList.contains('is-active')){
-        menu.classList.remove('is-active');
-      } else {
-        menu.classList.add('is-active');
-      }
-    })
-  }
+
 }
