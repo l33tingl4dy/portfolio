481c995194345db32604c1cf0a2065c0922065d1
Amanda Harris <alece.fwb@gmail.com>
2020-10-18 11:11:54 -0500

    fixing path names

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,11 @@ import { ResumeComponent } from './resume/resume.component';
 
 const routes: Routes = [
   // TODO: Add rerouting to home page when unknown path is entered
-  { path: '', redirectTo: 'home-component', pathMatch: 'full'},
-  { path: 'home-component', component: HomeComponent },
-  { path: 'cv-component', component: ResumeComponent},
-  { path: 'about-component', component: AboutPageComponent },
-  { path: 'contact-component', component: ContactPageComponent }
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'cv', component: ResumeComponent},
+  { path: 'about', component: AboutPageComponent },
+  { path: 'contact', component: ContactPageComponent }
 ];
 
 @NgModule({
