c029f010bf144450eb6e96ae6486229bb1de1afe
Amanda Harris <alece.fwb@gmail.com>
2020-12-04 18:28:55 -0600

    added running link and data to About Me

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -10,6 +10,15 @@ export class AboutPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.calculateRunningDay();
   }
 
+  public runDay = 0;
+
+  public calculateRunningDay() {
+    var startDate = new Date('December 31, 2018 00:00:00')
+    var today = new Date();
+    var timeDiff = today.getTime() - startDate.getTime();
+    this.runDay = Math.round(timeDiff / (1000 * 3600 * 24));
+  }
 }
