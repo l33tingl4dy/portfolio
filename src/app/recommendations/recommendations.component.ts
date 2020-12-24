8f007015936f1ea1c5c834305544785975a51224
Amanda Harris <alece.fwb@gmail.com>
2020-12-23 19:09:42 -0600

    adding recommendations to arrays

--- src/app/recommendations/recommendations.component.ts
+++ src/app/recommendations/recommendations.component.ts
@@ -10,6 +10,27 @@ export class RecommendationsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.setBookRecommendations();
   }
 
+
+  setBookRecommendations() {
+    let fictionBooks = [
+      { "id": 0, "name": "", "author": "", "amazon": "", "imgLink": "" },
+      { "id": 1, "name": "", "author": "", "amazon": "", "imgLink": "" },
+      { "id": 2, "name": "", "author": "", "amazon": "", "imgLink": "" },
+      { "id": 3, "name": "", "author": "", "amazon": "", "imgLink": "" },
+      { "id": 4, "name": "", "author": "", "amazon": "", "imgLink": "" },
+      { "id": 5, "name": "", "author": "", "amazon": "", "imgLink": "" },
+      { "id": 6, "name": "", "author": "", "amazon": "", "imgLink": "" }
+    ];
+    let nonFictionBooks = [
+      { "id": 0, "name": "", "author": "", "amazon": "", "imgLink": "" },
+      { "id": 1, "name": "", "author": "", "amazon": "", "imgLink": "" },
+      { "id": 2, "name": "", "author": "", "amazon": "", "imgLink": "" },
+      { "id": 3, "name": "", "author": "", "amazon": "", "imgLink": "" },
+      { "id": 4, "name": "", "author": "", "amazon": "", "imgLink": "" },
+      { "id": 5, "name": "", "author": "", "amazon": "", "imgLink": "" },
+    ]
+  }
 }
