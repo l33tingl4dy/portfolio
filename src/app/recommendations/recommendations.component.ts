912d1be8ef7d2c9f1ab8288cae4d71cd237d1006
Amanda Harris <alece.fwb@gmail.com>
2020-12-29 17:51:46 -0600

    adding images to recommendations

--- src/app/recommendations/recommendations.component.ts
+++ src/app/recommendations/recommendations.component.ts
@@ -18,18 +18,18 @@ public games = [];
 
   setRecommendations() {
     this.fictionBooks = [
-      { "name": "The Girl With All The Gifts", "author" :"M.R. Carey", "amazon": "", "imgLink": "" },
-      { "name": "Scythe", "author": "Neal Shusterman", "amazon": "", "imgLink": "" },
-      { "name": "We Were Liars", "author": "E. Lockhart", "amazon": "", "imgLink": "" },
-      { "name": "Ready Player One", "author": "Ernest Cline", "amazon": "", "imgLink": "" },
-      { "name": "They Both Die at the End", "author": "Adam Silvera", "amazon": "", "imgLink": "" },
-      { "name": "The Handmaid's Tale", "author": "Margaret Atwood", "amazon": "", "imgLink": "" },
+      { "name": "The Girl With All The Gifts", "author" :"M.R. Carey", "amazon": "https://smile.amazon.com/dp/0316334758/ref=cm_sw_r_tw_dp_x_q576Fb37JTXNB", "imgLink": "https://images-na.ssl-images-amazon.com/images/I/51an6r+508L._SX325_BO1,204,203,200_.jpg" },
+      { "name": "The Arc of a Scythe Trilogy", "author": "Neal Shusterman", "amazon": "https://smile.amazon.com/dp/153446154X/ref=cm_sw_r_tw_dp_x_-576FbZBZAP95", "imgLink": "https://images-na.ssl-images-amazon.com/images/I/91uDRSjlgDL.jpg" },
+      { "name": "We Were Liars", "author": "E. Lockhart", "amazon": "https://smile.amazon.com/dp/0385741278/ref=cm_sw_r_tw_dp_x_g876Fb8G95YQJ", "imgLink": "https://images-na.ssl-images-amazon.com/images/I/51Qhv7jm4uL._SX331_BO1,204,203,200_.jpg" },
+      { "name": "Ready Player One", "author": "Ernest Cline", "amazon": "https://smile.amazon.com/dp/0307887448/ref=cm_sw_r_tw_dp_x_X976FbW8GHTH1", "imgLink": "https://images-na.ssl-images-amazon.com/images/I/51UrTnJIgcL._SX322_BO1,204,203,200_.jpg" },
+      { "name": "They Both Die at the End", "author": "Adam Silvera", "amazon": "https://smile.amazon.com/dp/0062457802/ref=cm_sw_r_tw_dp_c976FbK2S6BMH?_x_encoding=UTF8&psc=1", "imgLink": "https://images-na.ssl-images-amazon.com/images/I/51zJppJNkkL._SX330_BO1,204,203,200_.jpg" },
+      { "name": "The Handmaid's Tale", "author": "Margaret Atwood", "amazon": "https://smile.amazon.com/dp/038549081X/ref=cm_sw_r_tw_dp_x_K-76FbVJHW56B", "imgLink": "https://images-na.ssl-images-amazon.com/images/I/41p0u2G9hbL._SX322_BO1,204,203,200_.jpg" },
       // { "name": "", "author": "", "amazon": "", "imgLink": "" }
     ];
     this.nonFictionBooks = [
       { "name": "HTML and CSS & Javascript and Jquery", "author": "Jon Ducket", "amazon": "https://smile.amazon.com/dp/1118907442/ref=cm_sw_r_tw_dp_x_eys5FbMVK0M74", "imgLink": "https://images-na.ssl-images-amazon.com/images/I/41T53nRtyoL._SX258_BO1,204,203,200_.jpg" },
-      { "name": "Let's Pretend This Never Happened", "author": "Jenny Lawson", "amazon": "", "imgLink": "" },
-      { "name": "Furiously Happy", "author": "Jenny Lawson", "amazon": "", "imgLink": "" },
+      { "name": "Let's Pretend This Never Happened", "author": "Jenny Lawson", "amazon": "https://smile.amazon.com/dp/0425261018/ref=cm_sw_r_tw_dp_x_Q376FbGJA46ET", "imgLink": "https://images-na.ssl-images-amazon.com/images/I/51fDtO2McLL._SX331_BO1,204,203,200_.jpg" },
+      { "name": "Furiously Happy", "author": "Jenny Lawson", "amazon": "https://smile.amazon.com/dp/1250077028/ref=cm_sw_r_tw_dp_x_J376FbSC3W5PH", "imgLink": "https://images-na.ssl-images-amazon.com/images/I/51x7psLF7NL._SX331_BO1,204,203,200_.jpg" },
       // { "id": 3, "name": "", "author": "", "amazon": "", "imgLink": "" },
       // { "id": 4, "name": "", "author": "", "amazon": "", "imgLink": "" },
     ]
