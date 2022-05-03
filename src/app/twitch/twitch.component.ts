423f5fcd6922adec85724f4fb81128339c48c084
Amanda Harris <alece.fwb@gmail.com>
2022-05-03 16:34:45 -0500

    adding incentives

--- src/app/twitch/twitch.component.ts
+++ src/app/twitch/twitch.component.ts
@@ -12,6 +12,18 @@ export interface IDonations {
   styleUrls: ['./twitch.component.css']
 })
 export class TwitchComponent implements OnInit {
+  public donations: IDonations[] =[{
+    donor: "Sionelle",
+    donationAmount: "$25"
+  },
+  {
+    donor: "Yugitoe",
+    donationAmount: "$100",
+    claimedReward: "Phasmo: Nightmare Run",
+    clipLink: "https://www.twitch.tv/videos/1472576562"
+  }
+
+]
   public highlights = [];
   constructor() { }
 
@@ -20,20 +32,7 @@ export class TwitchComponent implements OnInit {
   }
 
   getDonations() {
-    let donations: IDonations
-    [
-      {
-        donor: "Sionelle",
-        donationAmount: "$25"
-      },
-      {
-        donor: "Yugitoe",
-        donationAmount: "$100",
-        claimedReward: "Phasmo: Nightmare Run",
-        clipLink: ""
-      }
-    ]
-    this.highlights.push(donations);
+    this.highlights.push(this.donations);
     console.table(this.highlights);
   }
 
