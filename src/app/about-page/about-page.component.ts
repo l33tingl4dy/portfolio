0e55b8fb0577a84e1439d3fc1be000bf4bbf9621
Amanda Harris <alece.fwb@gmail.com>
2025-01-17 17:44:05 -0600

    minor updates + updated year

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -5,20 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about-page.component.html',
   styleUrls: ['./about-page.component.css']
 })
-export class AboutPageComponent implements OnInit {
+export class AboutPageComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-    this.calculateRunningDay();
-  }
-
-  public runDay = 0;
-
-  public calculateRunningDay() {
-    var startDate = new Date('December 31, 2018 00:00:00')
-    var today = new Date();
-    var timeDiff = today.getTime() - startDate.getTime();
-    this.runDay = Math.round(timeDiff / (1000 * 3600 * 24));
-  }
 }
--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+export interface IProjectInfo {
+  name: string,
+  dates: string,
+  tools: string,
+  img: string,
+  details: string
+}
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css']
 })
-export class ResumeComponent implements OnInit {
 
+export class ResumeComponent implements OnInit {
+  public projects: IProjectInfo[] = [{
+    name: '',
+    dates: '',
+    tools: '',
+    img: '',
+    details: ''
+  }, {
+    name: '',
+    dates: '',
+    tools: '',
+    img: '',
+    details: ''
+  }]
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  setProjects() {
+  }
 }
